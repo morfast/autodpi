bd51085d08729617ad06e730db67fc081b735703
morfast <morfast@example.com>
2017-04-19 17:03:51 +0800

    one class svm test

--- go.py
+++ go.py
@@ -1,30 +1,40 @@
 #!/usr/bin/python
 
 import sys
+import glob
+from numpy import mean
+import argparse
+
 from sklearn import svm
 from sklearn.model_selection import cross_val_score
-import glob
+from sklearn.linear_model import SGDClassifier
+from sklearn import neighbors
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn import tree
 
 
 # truncate Truncate_size bytes from the front
-Truncate_size = 3
+Truncate_size = 5
 
 def read_data_from_file(filename):
 
     global Truncate_size
 
+    skip_size = 0
+
     f = open(filename, "rb")
     rawdata = f.read()
-    if len(rawdata) < Truncate_size:
+    if len(rawdata) < skip_size + Truncate_size:
         return []
-    return [ord(b) for b in rawdata[:Truncate_size]]
+    return [ord(b) for b in rawdata[skip_size:skip_size+Truncate_size]]
 
-def read_data():
+def read_data(dirnames):
     """ read data from tcpflow output files """
 
     data = []
     label = []
-    for dirname in sys.argv[1:]:
+    for dirname in dirnames:
         dirname = dirname.strip('/')
         # get data file list
         filelist = glob.glob(dirname + "/*-*")
@@ -48,16 +58,56 @@ def read_data():
 
     return data, label
 
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--train", action='store', nargs='*')
+    parser.add_argument("-d", "--data", action='store', nargs='*')
+
+    args =  parser.parse_args()
+
+    return args
 
 def main():
     global Truncate_size
 
-    for Truncate_size in range(1,32):
+    for Truncate_size in range(1,16):
         x,y = read_data()
         
-        clf = svm.SVC()
+        #clf = svm.SVC()
+        #clf = SGDClassifier()
+        #clf = neighbors.KNeighborsClassifier(8, 'distance')
+        #clf = GaussianNB()
+        #clf = MLPClassifier(solver='lbfgs', alpha=1e-5, \
+        #                    hidden_layer_sizes=(5, 2), random_state=1)
+        clf = tree.DecisionTreeClassifier()
         clf.fit(x,y)
         scores = cross_val_score(clf, x, y, cv=5)
-        print scores
+        print mean(scores)
 
-main()
+def svmtest():
+    args = parse_arguments()
+
+    x,y = read_data(args.train)
+    data_dict = {}
+    for elem in zip(y,x):
+        key, value = elem
+        if data_dict.has_key(key):
+            data_dict[key].append(value)
+        else:
+            data_dict[key] = [value, ]
+
+    clfs = []
+    for key in data_dict.keys():
+        # key is type, value is data
+        clf = svm.OneClassSVM(nu=0.1, kernel="rbf", gamma=0.1)
+        #clf = svm.OneClassSVM()
+        clf.fit(data_dict[key])
+        clfs.append((key, clf))
+
+    x,y = read_data(args.data)
+    for clfname, clf in clfs:
+        print float(len([i for i in clf.predict(x) if i == 1]))/len(x)
+
+
+#main()
+clfs = svmtest()
